7513009e264ec57dac45eaebad4d75fe2213e875
seb4stien <seb4stien@georget.name>
2013-08-12 14:14:49 +0200

    quiet mode tests

--- freeplane-collaborative-addon.sikuli/freeplane-collaborative-addon.py
+++ freeplane-collaborative-addon.sikuli/freeplane-collaborative-addon.py
@@ -17,7 +17,10 @@ def doAction(action):
         popup("Unknown action")
 
 def doConfirm():
-    click("ok-button.png") 
+    if (exists("ok-button.png")):
+        click("ok-button.png") 
+    else:
+        click("1376306862302.png")
 
 def waitMindMapLong():
     sleep(1)
@@ -31,6 +34,45 @@ def waitMindMap():
 def closeMap():
     type("w", Key.CTRL)
 
+def setQuietMode(switch):
+    wait("1376226063138.png")
+    type(",", Key.CTRL)
+    wait("1376226110773.png")
+    click("1376226119369.png")
+    click("1376226128916.png")
+    modif = 0
+    if (switch == "on"):
+        if (exists(Pattern("1376226481281.png").exact())):
+            click(find(Pattern("1376226391315.png").targetOffset(44,0))) 
+            modif = 1
+    else:
+        if (exists(Pattern("1376226558204.png").exact())):
+            click(find(Pattern("1376226391315.png").targetOffset(44,0)))
+            modif = 1
+    click("1376226510613.png") 
+    sleep(1)
+    if (modif == 1):
+        doConfirm()
+
+def setVerboseMode(switch):
+    wait("1376226063138.png")
+    type(",", Key.CTRL)
+    wait("1376226110773.png")
+    click("1376226119369.png")
+    click("1376226128916.png")
+    modif = 0
+    if (switch == "on"):
+        if (exists(Pattern("1376306117551.png").exact())):
+            click(find(Pattern("1376306154468.png").targetOffset(50,0))) 
+            modif = 1
+    else:
+        if (not(exists(Pattern("1376306117551.png").exact()))):
+            click(find(Pattern("1376306154468.png").targetOffset(50,0))) 
+            modif = 1
+    click("1376226510613.png") 
+    sleep(1)
+    if (modif == 1):
+        doConfirm()
 
 ####################################
 # init
@@ -130,27 +172,37 @@ def testAddToVersionning(vcs):
 #####################################
 # up2date mindmap
 ####################################
-def testNothingToDo(vcs):
+def testNothingToDo(vcs, quiet):
     # init
     myApp = "C:\\My Program Files\\Freeplane\\freeplane.exe c:\\users\\dmxf8215\\documents\\personnel\\projets\\freeplane\\tests\\" + vcs + "-userA\\mindmaps\\tests.mm"
 
     openApp(myApp)
     waitMindMapLong()
 
-    actions = ['compare', 'update']
-    for action in (actions):
-        doAction(action)
+    doAction('compare')
+    wait("1373880970061.png", 10)
+    doConfirm()
+    waitMindMap()
+
+
+    doAction('update')
+    if (quiet):
+        wait("1376225689812.png", 10)
+    else: 
         wait("1373880970061.png", 10)
         doConfirm()
-        waitMindMap()
+    waitMindMap()
 
     doAction("commit")
 #    if vcs == "git":
 #        wait("1373882037155.png")
 #        paste("plop")
 #        click("1373882113393.png")       
-    wait("1373881045981.png", 10)
-    doConfirm()
+    if (quiet):
+        wait("1376225835742.png", 10)
+    else:
+        wait("1373881045981.png", 10)
+        doConfirm()
     waitMindMap()
     closeMap()
 
@@ -272,14 +324,17 @@ def testMerge(vcs, conflict):
         if vcs == "git":
             wait("1374162538809.png")
             doConfirm()
+            sleep(2)
             doAction("commit")
             wait("1373882037155.png")
             paste("plop")
             click("1373882113393.png")
             wait("1373927489627.png")
+            sleep(2)
             doConfirm()
             wait("1373926143621.png")
             click("1374162645369.png")
+            sleep(2)
             doAction("update")
             wait("1374163494960.png") 
         else:
@@ -308,27 +363,74 @@ def testMerge(vcs, conflict):
     type("s", Key.CTRL)    
     closeMap()
 
+
+def testLocalVersionningInQuietMode(vcs):
+    # init
+    myApp = "C:\\My Program Files\\Freeplane\\freeplane.exe c:\\users\\dmxf8215\\documents\\personnel\\projets\\freeplane\\tests\\" + vcs + "-noremote\\tests.mm"
+
+    openApp(myApp)
+    waitMindMapLong()
+
+    type("1373881357895.png", Key.ENTER)
+    paste("branch c") 
+    type(Key.ENTER)
+    sleep(1)
+    type("s", Key.CTRL)    
+
+    # compare    
+    doAction("commit")
+    wait("1373825742171.png")
+    click("1373825781197.png")
+    wait("1373825968949.png")
+    paste("plop")
+    doConfirm()
+    wait("1376230843086.png")
+
+    type("1373928278544.png", Key.INSERT)
+    paste("new leaf a.a.a")
+    type(Key.ENTER)
+    sleep(1)
+    type("s", Key.CTRL)    
+    
+    doAction("commit")
+    wait("1373825968949.png")
+    paste("plop")   
+    doConfirm()    
+    wait("1376230843086.png")
+
+    doAction("update")
+    wait("1376306904240.png")   
+                
 #
 # Main
 #
 
 
+#setQuietMode("off")
+#setVerboseMode("off")
 #testNoVersionning()
 
 #testFakeVersionning('cvs')
 #testAddToVersionning('cvs')
-#testNothingToDo('cvs')
+#testNothingToDo('cvs', 0)
 #testMerge('cvs', 'false')
 #testMerge('cvs', 'true')
 
 #testFakeVersionning('svn')
 #testAddToVersionning('svn')
-#testNothingToDo('svn')
+#testNothingToDo('svn', 0)
 #testMerge('svn', 'false')
 #testMerge('svn', 'true')
-#
+
 #testFakeVersionning('git')
 #testAddToVersionning('git')
-#testNothingToDo('git')
-testMerge('git', 'false')
-testMerge('git', 'true')
+#testNothingToDo('git', 0)
+#testMerge('git', 'false')
+
+#setQuietMode("on")
+#testNothingToDo('cvs', 1)
+#testNothingToDo('svn', 1)
+#testNothingToDo('git', 1)
+testLocalVersionningInQuietMode('git')
+
+#testMerge('git', 'true')
